c6eb51364350b92a2d7b6a7e46c17e68bad0652c
Todd <twitzel@gmail.com>
2017-02-09 16:34:02 -0700

    Cleaned up webserver

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 ll = require('./llibR5');
 ll.loadSettings(settingsLoaded)
 function settingsLoaded(){
-    console.log('settings.txt file loaded');
+    console.log('settings.txt file loaded to global.settings');
     ll.loadWiz(wizLoaded)
 }
 function wizLoaded(){
 //    console.log(JSON.stringify(global.wiz))
-    console.log('wiz.dat file loaded');
+    console.log('wiz.dat file loaded to global.wiz');
 }
