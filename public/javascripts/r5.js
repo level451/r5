4b54dc9d84d844518d402be2270ea637dbf1d315
Todd <twitzel@gmail.com>
2017-02-11 20:32:44 -0700

    Cleaned up webserver

--- public/javascripts/r5.js
+++ public/javascripts/r5.js
@@ -1,12 +1,13 @@
 var pagename ='r5';
 var events = [];
 var inttimer = null;
+var offset = 0;
+
 
 
 
 function load() {
 
-    wsUri = "ws://" + window.location.hostname + ":<%=wsport%>";
     canvas = document.getElementById('canvas')
     canvas.width=window.outerWidth
     canvas.height=(window.outerWidth*(3/4)); // aspect ratio set to 16/9
@@ -14,17 +15,68 @@ function load() {
     // draw the welcome image
     ctx = canvas.getContext('2d');
     var welcomeImage = new Image();
+
     welcomeImage.onload = function(){
-        ctx.drawImage(welcomeImage,0,0,canvas.width,canvas.height)
+   //     ctx.drawImage(welcomeImage,0,0,canvas.width,canvas.height)
+        languageList = getLanguages();
+        var t0 = performance.now();
+        drawMenuText(languageList,0);
+        var t1 = performance.now();
+        console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.")
+
+
+         int = setInterval(function(){
+            offset+=1;
+             var t0 = performance.now();
+             drawMenu(offset);
+             var t1 = performance.now();
+            // console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.")
+
+            if (offset > 100){clearInterval(int)}
+        },20)
     }
     //welcomeImage.src = 'show/'+wiz.ShowName+'/Welcome.jpg'
     welcomeImage.src = 'show/Welcome.jpg'
     websockstart();
 
+
 }
-function drawLanguageMenu(){
+function getLanguages(){
+    var rv = []
+    for (var key in wiz) {
+        if (key.indexOf('Service') == 0  ){
+            rv.push(wiz[key])
+        }
+
+    }
+    return rv
+}
+function drawMenu(offset){
+    ctx.putImageData(menu,0,offset)
+}
 
 
+function drawMenuText(list,item){
+const itemsToDisplay = 5
+    ;
+var counter = 1;
+    ctx.font = (200/itemsToDisplay)*scale+'px sans-serif'
+
+for (var i = item-(Math.floor(itemsToDisplay/2));i<itemsToDisplay-(Math.floor(itemsToDisplay/2));++i)
+    //for (var i = 0;i<list.length;++i)
+
+{
+    if(list[i] != undefined){
+        ctx.fillText(list[i],(canvas.width/2)-(ctx.measureText(list[i]).width/2),(counter*(canvas.height/(itemsToDisplay+1)))+20*scale);
+        console.log((canvas.width/2)-(ctx.measureText(list[i]).width/2))
+    }
+
+
+    console.log(list[i])
+    ++counter
+}
+//menu = ctx.getImageData(0,0,canvas.width,list.length*(canvas.height/(itemsToDisplay+1)));
+  //  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 }
 function getFont(fontSize) {
