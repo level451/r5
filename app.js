ce49a994f2604beb9e934d1dcbfe6a36082fcf29
stevewitz <switzel@wizcomputing.coom>
2017-04-25 18:04:26 -0700

    Added 2 Jpgs and package.json

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ function settingsLoaded(){
     console.log('settings.txt file loaded to global.settings');
     ll.loadWiz(wizLoaded)
 }
+
 function wizLoaded(){
 //    console.log(JSON.stringify(global.wiz))
     console.log('wiz.dat file loaded to global.wiz');
