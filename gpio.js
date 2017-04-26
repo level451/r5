f6105f231c9b96abfbedf60be7ff7a0b7bc721c2
stevewitz <switzel@wizcomputing.coom>
2017-04-26 14:48:32 -0700

    gpio changed to rpi-gpio

--- gpio.js
+++ gpio.js
@@ -21,11 +21,12 @@ exports.pintest = function () {
 
 exports.pwm = function(){
 
-    gpio.setup(7, gpio.DIR_IN, readInput);
+    gpio.setup(7, gpio.DIR_OUT, write);
 
-    function readInput() {
-        gpio.read(7, function(err, value) {
-            console.log('The value is ' + value);
+    function write() {
+        gpio.write(7, true, function(err) {
+            if (err) throw err;
+            console.log('Written to pin');
         });
     }
 }
