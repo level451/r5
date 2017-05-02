253b6b39cf2f0f594bcea9be97d00d589942480d
Todd <twitzel@gmail.com>
2017-05-02 14:23:26 -0600

    Added cue processor

--- cueprocessor.js
+++ cueprocessor.js
@@ -3,10 +3,33 @@
  */
 var debug = true;
 var console = {}
-console.log = (function () {return function (x) {if (debug) {process.stdout.write(ll.ansitime('green','Cue: ') + x + '\n');}}})();
+console.log = (function () {return function (x) {if (debug) {process.stdout.write(ll.ansitime('green','Cue           ') + x + '\n');}}})();
 
 
 exports.incommingCue = function(c){
-console.log(c)
+    //temp line for testing - remove when cue sended is fixed
+c= wiz.ShowName+' '+'GO '+c
+
+    c = c.match(/\S+/g); // breaks string into array at the spaces so s[0] is showname etc.
+    // verify the showname is the correct showname
+    if (c[0].toUpperCase() != wiz.ShowName.toUpperCase()){
+        process.stdout.write(ll.ansitime('red','Cue           Showname error RECIEVED:'+c[0]+' EXPECTED:'+wiz.ShowName)+'\n'); // show error
+        return;
+    }
+
+    switch (c[1])
+    {
+        case 'GO':
+            break;
+        default:
+            process.stdout.write(ll.ansitime('red','Cue           Command not known:'+c[1])+'\n'); // show error
+            return
+            break;
+
+
+
+    }
+
+    console.log(c)
 
 }
