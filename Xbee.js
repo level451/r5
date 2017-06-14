c6d8db550c02be8c4b32562fbcccc2b30f53f7bf
stevewitz <switzel@wizcomputing.coom>
2017-06-14 16:29:44 -0700

    xbee ready to test

--- Xbee.js
+++ Xbee.js
@@ -2,50 +2,128 @@
  * Created by steve on 6/14/2017.
  */
 const llib = reguire("./llibR6");
-var state = 0;
+var state;
 var timer;   setTimeout(timedout, 1000);
-var noresponse = 0;
 var data;
 // state ss1 - signal strength 1 - waiting for confirmation that xbee is in AT mode
 
 exports.xbeeReceivedData = function(returnedData){
     clearTimeout(timer); //clear the timer as we have data
-    switch (state){
-
-
-
-
+    switch (state.substr(0,2)){
+        case "ss":
+            exports.xbeeGetsignalStrength(state, returnedData);
+            break;
+        case "gi":
+            exports.xbeeGetPanID(state, returnedData);
+            break;
+        case "si":
+            exports.xbeeSetPanID(state, returnedData);
+            break;
     }
 
 
 }
 
-exports.xbeeGetsignalStrength = function(st){
+exports.xbeeGetsignalStrength = function(st,data){
 
     switch(st){
         case 666:
             console.log("ERROR from XBEE SS");
             break;
         case 0:
-            sendXbeeData(ss1,"+++");
+            sendXbeeData("ss1","+++");
             break;
         case "ss1":
             if(data = "OK"){
-              sendXbeeData(ss2,"ATID\r");
+              sendXbeeData("ss2","ATDB\r");
             }
+            else{
+                exports.xbeeGetsignalStrength(666,"error"); //if we dont get ok then something is wrong
+            }
+            break;
+
+        case "ss2"://this data is the signal strength
+            console.log("The Signal Strength is: " + data);
+            sendXbeeData("ss3","ATCN\r");//clear AT mode
+            break;
 
     }
 
 
 }
 
-exports.xbeeGetPanID = function(st){
+exports.xbeeGetPanID = function(st,data){
+    switch(st){
+        case 666:
+            console.log("ERROR from XBEE GI");
+            break;
+        case 0:
+            sendXbeeData("gi1","+++");
+            break;
+        case "gi1":
+            if(data == "OK"){
+                sendXbeeData("gi2","ATID\r");
+            }
+            else{
+                exports.xbeeGetsignalStrength(666,"error"); //if we dont get ok then something is wrong
+            }
+            break;
 
+        case "gi2"://this data is the pan ID
+            console.log("The Pan ID is: " + data);
+            sendXbeeData("gi3","ATCN\r");//clear AT mode
+            break;
+
+
+    }
 
 }
 
-exports.xbeeSetPanID = function(st, id){
+exports.xbeeSetPanID = function(st,data,id){
+    switch(st){
+        case 666:
+            console.log("ERROR from XBEE SI");
+            break;
+        case 0:
+            sendXbeeData("si1","+++");
+            break;
+        case "si1":
+            if(data == "OK"){
+                sendXbeeData("si2","ATID" + id + "\r"); // send out the new pan id
+            }
+            else{
+                exports.xbeeGetsignalStrength(666,"error"); //if we dont get ok then something is wrong
+            }
+            break;
 
+        case "si2"://this data is confirmation of pan id
+            if(data == "OK") {
+                sendXbeeData("si3", "ATAC\r");//Apply changes
+            }
+            else{
+                exports.xbeeSetPanID(666,"error"); //if we dont get ok then something is wrong
+            }
+            break;
+        case "si3"://this data is confirmation of pan id
+            if(data == "OK") {
+                sendXbeeData("si4", "ATWR\r"); //Make changes permanent
+            }
+            else{
+                exports.xbeeSetPanID(666,"error"); //if we dont get ok then something is wrong
+            }
+            break;
+        case "si4"://this data is confirmation of pan id
+            if(data == "OK") {
+                sendXbeeData("si5", "ATCN\r"); //exit AT mode
+            }
+            else{
+                exports.xbeeSetPanID(666,"error"); //if we dont get ok then something is wrong
+            }
+            break;
+
+
+
+    }
 
 
 }
@@ -57,14 +135,9 @@ function sendXbeeData(st, data){
     setTimeout(timedout, 1000);
 }
 
-function exitATMode(){
 
-
-}
-
-function timedout(){
-    noresponse = 1; // the 1 second timer has timed out send error back;
-    switch (state.substring(0,2)){
+function timedout(){ // we are here because the timer timed out - must be an error
+    switch (state.substr(0,2)){
         case ss:
             exports.xbeeGetsignalStrength(666);
             break;
@@ -77,6 +150,5 @@ function timedout(){
     }
 }
 function setTimedout(){
-    noresponse = 0;// clear the variable
     timer = setTimeout(timedout,1000);
 }
