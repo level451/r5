a5c278780990ac757f7c6b2532f19304c0c24e84
Todd <twitzel@gmail.com>
2017-06-22 16:52:37 -0600

    System menu:Unit Status added

--- llibR6.js
+++ llibR6.js
@@ -287,6 +287,8 @@ exports.getUnitSettings = function(){
             }
             var Pan = xbee.xbeeGetPanID(0,0,function(Pan){
                 console.log("Battery: " + global.Battery + " Temperature: " + global.Temperature + " Pan ID: " + Pan );
+                ws.send(JSON.stringify({object:'unitStatus',data:{Battery:global.Battery,Pan: Pan',Signal:'NA',Temperature:global.Temperature}}),'r6');
+
             });
 
         });
