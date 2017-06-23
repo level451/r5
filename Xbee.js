c8292c976bf60d5ed292db9f265f48e6878a8fdf
stevewitz <switzel@wizcomputing.coom>
2017-06-23 12:09:11 -0700

    working on getunitstatus

--- Xbee.js
+++ Xbee.js
@@ -45,7 +45,7 @@ exports.xbeeGetsignalStrength = function(st,data,cb){
             console.log('Entering AT mode')
             setTimeout(function(){
                 sendXbeeData("ss1","+++");
-            }, 500);
+            }, 1000);
 
 
             break;
