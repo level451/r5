093bfcf6d83cfe482d48ff266da2bf0c3c5a0f49
stevewitz <switzel@wizcomputing.coom>
2017-06-27 14:23:59 -0700

    backlight

--- llibR6.js
+++ llibR6.js
@@ -323,11 +323,11 @@ exports.backlight = function(value,direction){
     console.log("ontime:" + wiz.OnTime);
     console.log("fadein:" + wiz.FadeIn);
     console.log("fadeout:" + wiz.FadeOut);
-
+    console.log("wiz.Backlight * backlightNanoPimax " + wiz.Backlight*backlightNanoPiMax);
     console.log("Backlight request: " + backlightLevel + "  direction is: " + direction)
     if(direction == 'up'){
        backlightLevel +=1;
-        if(backlightLevel > wiz.Backlight*backlightNanoPiMax){
+        if(backlightLevel >= wiz.Backlight*backlightNanoPiMax){
             clearInterval(timerBacklightOn);
         }
     }
