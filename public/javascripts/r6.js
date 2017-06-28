66d47a470d8bb505e708f21b01817d19d89198d5
stevewitz <switzel@wizcomputing.coom>
2017-06-28 08:10:19 -0700

    hidded scroll bars  r6.js line 28 and none on cursor

--- public/javascripts/r6.js
+++ public/javascripts/r6.js
@@ -25,7 +25,8 @@ function load() {
     }
     disp.style.width = settings.webPage.width + "px";
     disp.style.height = settings.webPage.height +"px";
-
+    disp.style.overflow = hidden;
+    siap.style.cursor = none;
     disp.style.transform = "rotate(" + settings.webPage.rotation + "deg)";
 
     canvas = document.getElementById('canvas');
@@ -40,7 +41,7 @@ function load() {
 //   canvas.height=(window.outerWidth*(.5625)); // aspect ratio set to 16/9
     scale = settings.webPage.width/320 ;// //320 is the default indow size - everything will be scaled according to this
 
-    document.body.style.overflow = "hidden";
+
     document.addEventListener('keydown', function(evt) {
         switch (evt.key){
             case 'a':
