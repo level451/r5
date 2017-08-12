01fd21790b556822815b43b13dd56ecd33d5032f
stevewitz <switzel@wizcomputing.coom>
2017-08-12 12:40:22 -0700

    updated battery to update at an interval

--- llibR6.js
+++ llibR6.js
@@ -53,62 +53,62 @@ function updateBattTemp() {
 }
 
 function updateBattery(){
-    fs.readFile(battADC, 'utf8', (err,filetxt) => {
-        if (err) {
-            console.log("Battery ERROR:  " + err );
-        }
-        else {
-                battVoltage += parseInt(filetext);
-                battCounter ++;
-            console.log("Raw ADC Value: ",filetxt );
-        }
-    });
-        if(battCounter = 20) {
-
-            clearInterval(battTimer);
-            battVoltage = battVoltage/battCounter; //get the average reading
-            battCounter = 0; //clear it so we can start over
-
-            global.Battery = (battVoltage * .003310466).toFixed(2);
-            battVoltage = 0;//now that we have reading, clear it
-            console.log("Batt Averaged Value: " + global.Battery);
-
-            // ###########################################################################################################
-            // ###########################################################################################################
-
-            //convert to percentage of battery --- this is totally arbitrary - need to put in real life values from testing
-
-            // ###########################################################################################################
-            // ###########################################################################################################
-
-
-            if (global.Battery > 4) {
-                global.Battery = 100;
-            }
-            else if (global.Battery > 3.9) {
-                global.Battery = 90;
-            }
-            else if (global.Battery > 3.8) {
-                global.Battery = 75;
-            }
-            else if (global.Battery > 3.4) {
-                global.Battery = 50;
-            }
-            else if (global.Battery > 3) {
-                global.Battery = 25;
-            }
-            else if (global.Battery > 2.8) {
-                global.Battery = 10;
-            }
-            else if (global.Battery > 2.7) {
-                global.Battery = 5;
-            } else {
-                global.Battery = 80 // default vaule if not read
-            }
-
-
-            console.log("Battery Voltage: " + global.Battery);
-        }
+    // fs.readFile(battADC, 'utf8', (err,filetxt) => {
+    //     if (err) {
+    //         console.log("Battery ERROR:  " + err );
+    //     }
+    //     else {
+    //             battVoltage += parseInt(filetext);
+    //             battCounter ++;
+    //         console.log("Raw ADC Value: ",filetxt );
+    //     }
+    // });
+    //     if(battCounter = 20) {
+    //
+    //         clearInterval(battTimer);
+    //         battVoltage = battVoltage/battCounter; //get the average reading
+    //         battCounter = 0; //clear it so we can start over
+    //
+    //         global.Battery = (battVoltage * .003310466).toFixed(2);
+    //         battVoltage = 0;//now that we have reading, clear it
+    //         console.log("Batt Averaged Value: " + global.Battery);
+    //
+    //         // ###########################################################################################################
+    //         // ###########################################################################################################
+    //
+    //         //convert to percentage of battery --- this is totally arbitrary - need to put in real life values from testing
+    //
+    //         // ###########################################################################################################
+    //         // ###########################################################################################################
+    //
+    //
+    //         if (global.Battery > 4) {
+    //             global.Battery = 100;
+    //         }
+    //         else if (global.Battery > 3.9) {
+    //             global.Battery = 90;
+    //         }
+    //         else if (global.Battery > 3.8) {
+    //             global.Battery = 75;
+    //         }
+    //         else if (global.Battery > 3.4) {
+    //             global.Battery = 50;
+    //         }
+    //         else if (global.Battery > 3) {
+    //             global.Battery = 25;
+    //         }
+    //         else if (global.Battery > 2.8) {
+    //             global.Battery = 10;
+    //         }
+    //         else if (global.Battery > 2.7) {
+    //             global.Battery = 5;
+    //         } else {
+    //             global.Battery = 80 // default vaule if not read
+    //         }
+    //
+    //
+    //         console.log("Battery Voltage: " + global.Battery);
+    //     }
 }
 
 getMACAddress(); // gets Mac address to gloabel.Mac
