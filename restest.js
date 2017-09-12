8c95267ae4d32c5caa48ee3a4ee1c4988e554d50
Todd <twitzel@gmail.com>
2017-09-12 11:54:43 -0600

    added restest - to test screen resolution

--- restest.js
+++ restest.js
@@ -2,7 +2,8 @@
  * Created by todd on 9/12/2017.
  */
 
-require('child_process').exec('xrandr', function (err, resp) {
+require('child_process').exec("xdpyinfo  | grep 'dimensions:'", function (err, resp) {
+    var resolution = resp.substring(resp.charAt(':'),resp.charAt('p')).trim()
     console.log(resp)
     console.log(err)
 
