d9bc7768ce274a899a1b36f2407e7d1f751dd87e
Todd <twitzel@gmail.com>
2017-09-12 12:13:09 -0600

    added restest - to test screen resolution

--- restest.js
+++ restest.js
@@ -3,6 +3,7 @@
  */
 
 require('child_process').exec("xdpyinfo  | grep 'dimensions:'", function (err, resp) {
+    resp = resp.toString()
     var resolution = resp.substring(resp.charAt(':'),resp.charAt('p')).trim()
     console.log(resp)
     console.log(resolution)
