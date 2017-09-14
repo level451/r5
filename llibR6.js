e6cf81418fac5e3455b71f029992225e17001ac7
Todd <twitzel@gmail.com>
2017-09-13 21:49:24 -0600

    new settings missing handler

--- llibR6.js
+++ llibR6.js
@@ -711,9 +711,9 @@ exports.wifiCheck = function(){
 function getAccessPoints(cb){
 
     require('child_process').exec('iwlist wlan0 scan | grep "ESSID"', function (err, resp) {
+        console.log(resp)
         var rv = resp.replace(/ESSID:/g,'')
-        rv = rv.split('/n')
-        return cb(rv)
+        return cb(rv.split('/n'))
     });
 }
 exports.wifiandPanIdcheckandset= function(){
