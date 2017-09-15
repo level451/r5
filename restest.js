3fa1ebd38e94b9514480ea6f7b043b3a60d9ac88
stevewitz <switzel@wizcomputing.coom>
2017-09-15 16:12:26 -0700

    added usb detection to test

--- restest.js
+++ restest.js
@@ -5,7 +5,7 @@
 var monitor = require('node-usb-detection');
 
 
-console.log("Usb Devices:\n", monitor.list());
+//console.log("Usb Devices:\n", monitor.list());
 
 monitor.add(function(device) {
     console.log("added device:\n", device);
@@ -15,9 +15,9 @@ monitor.remove(function(device) {
     console.log("removed device:\n", device);
 });
 
-monitor.change(function(device) {
-    console.log("device changed:\n", device);
-});
+//monitor.change(function(device) {
+//    console.log("device changed:\n", device);
+//});
 
 
 //require('child_process').exec("xdpyinfo  | grep 'dimensions:'", function (err, resp) {
