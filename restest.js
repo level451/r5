955bb4652a18034b99ae2b195c9c79a9da2e4106
stevewitz <switzel@wizcomputing.coom>
2017-09-15 16:48:38 -0700

    added usb detection to test

--- restest.js
+++ restest.js
@@ -30,10 +30,10 @@ var usbDetect = require('usb-detection');
 
 // Detect add/insert
 usbDetect.on('add', function(device) { console.log('add', device); });
-usbDetect.on('add:vid', function(device) { console.log('add', device); });
-usbDetect.on('add:vid:pid', function(device) { console.log('add', device); });
+//usbDetect.on('add:vid', function(device) { console.log('add', device); });
+//usbDetect.on('add:vid:pid', function(device) { console.log('add', device); });
 
 // Detect remove
 usbDetect.on('remove', function(device) { console.log('remove', device); });
-usbDetect.on('remove:vid', function(device) { console.log('remove', device); });
-usbDetect.on('remove:vid:pid', function(device) { console.log('remove', device); });
+//usbDetect.on('remove:vid', function(device) { console.log('remove', device); });
+//usbDetect.on('remove:vid:pid', function(device) { console.log('remove', device); });
