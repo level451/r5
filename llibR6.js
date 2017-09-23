153e576ce72d93fb29c9184945101d1bc5f8f815
stevewitz <switzel@wizcomputing.coom>
2017-09-23 12:49:05 -0700

    changing backlight

--- llibR6.js
+++ llibR6.js
@@ -602,7 +602,8 @@ exports.backlightOff = function(){
 function backLightDown(){
     var steps = 20;
     var timerBacklightDown = [steps];
-    var exponent = Math.log(wiz.Backlight*backlightNanoPiMax/100)/Math.log(1.6) //find exponent of max value
+    var exponent = Math.log(wiz.Backlight*backlightNanoPiMax/100)/Math.log(1.6); //find exponent of max value
+    console.log("exponent: " + exponent);
     for (var i = 0; i < steps; i++){
         timerBacklightDown[i] = setTimeout(function(){exports.backlight(Math.pow(1.6, (exponent -i*exponent/steps))) -.5},((wiz.FadeOut*1000/steps)*i) +1 );
         console.log("calc: " + i + "value: "+Math.pow(1.6, (exponent -i*exponent/steps)) -.5) ;
