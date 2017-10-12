e62f1ff35d1546d388656160b9e94bdd4761a0cc
stevewitz <switzel@wizcomputing.coom>
2017-10-12 15:25:52 -0700

    playing with fade in

--- public/javascripts/r6.js
+++ public/javascripts/r6.js
@@ -1377,7 +1377,7 @@ function drawUnitStatus(unitinfo,data){
 }
 
 function HDMIFade(){
-    fps = 1000/60;
+    fps = 1000/30;
     opacity += 1/(fps*wiz.FadeIn);
     console.log(opacity);
     if(opacity >=1){
